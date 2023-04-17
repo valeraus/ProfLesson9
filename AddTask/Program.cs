6bff0480a95ba9ff22c6ce964eae46faab83037d
Valera <vlaus0@gmail.com>
2023-04-17 23:02:53 +0300

    Добавьте файлы проекта.

---
+++ AddTask/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace AddTask
+{
+    /*
+    Створіть власний клас, об’єкти котрого будуть займати багато місця в пам’яті 
+    (наприклад, в коді класу буде присутній великий масив) і реалізуйте для цього класу формалізований шаблон очищення.
+     */
+    class MyClass : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            CleanUp(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~MyClass()
+        {
+            Console.WriteLine("Finalise");
+            CleanUp(false);
+        }
+
+        private void CleanUp(bool clean)
+        {
+            if (!this.disposed)
+            {
+                if (clean)
+                {
+                    Console.Write("Освобождение ресурсов");
+
+                    for (int i = 0; i < 40; i++)
+                    {
+                        Console.Write("F");
+                    }
+                }
+                Console.WriteLine("Finalized");
+            }
+            this.disposed = true;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            using (var myClass = new MyClass())
+            {
+                Console.WriteLine(myClass);
+            }
+            Console.WriteLine(new string('-', 20));
+
+
+            var myClass2 = new MyClass();
+            Console.WriteLine(myClass2);
+
+            myClass2.Dispose();
+            myClass2.Dispose();
+            myClass2.Dispose();
+            myClass2.Dispose();
+
+            var myClass3 = new MyClass();
+
+            Console.ReadKey();
+            Console.WriteLine("Press any key to dispose");
+        }
+    }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace Task2
+{
+    /*
+    Створити клас, який дозволяє стежити за ресурсами, що використовуються програмою.
+    Використовуйте його для моніторингу роботи програми, а саме: 
+    користувач може вказати прийнятні рівні споживання ресурсів (пам'яті), 
+    а методи класу дадуть попередження, коли кількість фактично використовуваних ресурсів наближається до максимально допустимого рівня.
+     */
+    class MonitorMemory
+    {
+        readonly int memoryLimit;
+
+        public MonitorMemory(int memoryLimit)
+        {
+            this.memoryLimit = memoryLimit;
+        }
+
+        bool IsMemoryLimitExceeded()
+        {
+            return this.memoryLimit < GC.GetTotalMemory(false);
+        }
+
+        public void WarnIfMemoryLimitExceeded(object errorMessage)
+        {
+            if (IsMemoryLimitExceeded())
+            {
+                Console.WriteLine("{0}", errorMessage);
+            }
+        }
+
+    }
+
+    class LargeObject
+    {
+        int[] array = new int[100000000]; // 100 000 000 Б * 4 = 400 000 000 Б = 390 625 КБ = 381 МБ
+
+        public void Method(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Timer timer = new Timer(new MonitorMemory(100000000).WarnIfMemoryLimitExceeded, "Warning memory out", 0, 200);
+
+            LargeObject[] array = new LargeObject[1000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                new LargeObject().Method(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
